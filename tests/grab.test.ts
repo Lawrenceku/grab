d6e02be85a41a3f1d06b7ddfaa553c3ed632fc94
Lawrence <lawrencekunmilola@gmail.com>
2025-06-23 22:44:38 +0100

    fix config and tests

--- tests/grab.test.ts
+++ tests/grab.test.ts
@@ -1,7 +1,20 @@
+import { grab } from '../src/index';
 
-describe('Hello World', () => {
-  it('should return hello world', () => {
-    const message = 'Hello World';
-    expect(message).toBe('Hello World');
+describe('grab', () => {
+  it('should perform a GET request and return data', async () => {
+    const data = await grab('GET', { url: 'https://jsonplaceholder.typicode.com/posts/1' });
+    expect(data).toHaveProperty('id', 1);
+  });
+
+  it('should perform a POST request and return created data', async () => {
+    const postData = { title: 'foo', body: 'bar', userId: 1 };
+    const data = await grab('POST', {
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      body: postData,
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
+    });
+    expect(data).toHaveProperty('title', 'foo');
+    expect(data).toHaveProperty('body', 'bar');
+    expect(data).toHaveProperty('userId', 1);
   });
 });
